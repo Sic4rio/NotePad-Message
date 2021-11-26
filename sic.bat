38e2ed6b2ec0ad20b7a9b1185c55a70bf0f0ce43
Sic4rio <75425513+sic4rio@users.noreply.github.com>
2021-11-26 16:23:37 +1100

    Add files via upload

---
+++ sic.bat
@@ -0,0 +1,11 @@
+@ECHO off
+color 2
+:Begin
+msg * Hi
+msg * Are you having fun?
+msg * I am!
+msg * Lets have fun together!
+msg * Because you have been o-w-n-e-d
+msg * By THE SIC4RI0 
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+GOTO BEGIN
